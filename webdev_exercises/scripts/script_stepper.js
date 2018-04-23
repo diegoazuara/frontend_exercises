8e8fce857935fbb87d68698c144d592d0051305d
Diego Azuara <dazuara@mayantechs.com>
2018-04-23 13:51:39 -0500

    update

--- webdev_exercises/scripts/script_stepper.js
+++ webdev_exercises/scripts/script_stepper.js
@@ -1,10 +1,17 @@
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the crurrent tab
 
+var btn = document.getElementById("nextBtn");
+btn.addEventListener("click", function() {
+  nextPrev(1)
+}); //Function(){} for it to run after clicking
+
+
 function showTab(n) {
   // This function will display the specified tab of the form...
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
+
   //... and fix the Previous/Next buttons:
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
@@ -12,9 +19,9 @@ function showTab(n) {
     document.getElementById("prevBtn").style.display = "inline";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("nextBtn").textContent = "Submit";
   } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("nextBtn").textContent = "Next";
   }
   //... and run a function that will display the correct step indicator:
   fixStepIndicator(n)
@@ -34,7 +41,7 @@ function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
-
+  if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
@@ -47,4 +54,61 @@ function nextPrev(n) {
   }
   // Otherwise, display the correct tab:
   showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByClassName("form__input");
+  z = document.getElementById('email').value;
+
+  console.log(z);
+
+  if (reg.test(z) == false) {
+    y[1].className += " invalid";
+    valid = false;
+  }
+
+  // Confirms password
+
+  if (currentTab == 0) {
+
+    if (document.getElementById('password').value.length < 8) {
+      y[2].className += " invalid";
+      valid = false;
+    } else {
+
+      if (document.getElementById('password').value ==
+        document.getElementById('confirm_password').value) {
+        valid = true;
+      } else {
+        y[3].className += " invalid";
+        valid = false;
+      }
+    }
+
+  }
+
+
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+
+  if (reg.test(z) == false) {
+    y[1].className += " invalid";
+    valid = false;
+  }
+
+  return valid;
 }
