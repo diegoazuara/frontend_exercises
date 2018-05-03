e707feb1040a2d25ee223bc564f1d0a8cb92b411
MAYANTECHS\dazuara <d_azuara@live.com.mx>
2018-05-03 14:09:20 -0500

    -Added new sections -Added images -Scroll animations
    -Responsiveness

--- webdev_exercises/scripts/rest_api_lp.js
+++ webdev_exercises/scripts/rest_api_lp.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
   $('.header__btn').on('click', function() {
-    $('ul').toggleClass('show');
-
+    $('.container ul').toggleClass('show');
   });
+
+  var offset = function() {
+    return {
+      top: 0 - $('#nav').height()
+    };
+  };
+
+  $('#nav').localScroll({
+    duration: 800,
+    offset: offset
+  });
+
 });
 
 var animateHTMLFadeInLeft = function() {
@@ -61,5 +72,35 @@ var animateHTMLFadeIn = function() {
   }
 }
 
-animateHTMLFadeInLeft().init()
-animateHTMLFadeIn().init()
+var animateHTMLLightSpeedIn = function() {
+  var elems,
+    windowHeight
+  var init = function() {
+    elems = document.getElementsByClassName('hidden-light_speed_in')
+    windowHeight = window.innerHeight
+    _addEventHandlers()
+  }
+  var _addEventHandlers = function() {
+    window.addEventListener('scroll', _checkPosition)
+    window.addEventListener('resize', init)
+  }
+
+  var _checkPosition = function() {
+    for (var i = 0; i < elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().top
+      if (posFromTop - windowHeight <= 0) {
+
+        console.log(elems);
+        elems[i].className = elems[i].className.replace('hidden-light_speed_in', 'lightSpeedIn')
+      }
+    }
+  }
+  return {
+    init: init
+  }
+}
+
+
+animateHTMLFadeInLeft().init();
+animateHTMLFadeIn().init();
+animateHTMLLightSpeedIn().init();
